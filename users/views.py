1cd73c0f0e56d29d34bd6853d525cbf55d814880
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-06 14:35:55 +0200

    login and logout functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse_lazy
+
+from . import forms
+
+
+def login_user_view(request):
+    if request.method == 'POST':
+        form = forms.LoginForm(request, request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(email=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse_lazy('home:home'))
+
+    else:
+        form = forms.LoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect(reverse_lazy('users:login'))
