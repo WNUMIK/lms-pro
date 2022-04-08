28a05c7e19e9697b3f3b798785428899d42787d3
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-08 11:35:16 +0200

    add custom migrations for groups and superuser from envs

--- conftest.py
+++ conftest.py
@@ -7,5 +7,4 @@ def user(db, django_user_model):
     """User instance from default django user model
 
     """
-    Group.objects.create(name='students')
     yield django_user_model.objects.create_user(email='a@a.pl', fullname='Ala', password='testPass123')
